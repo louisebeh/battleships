c9a92fdf87a7ee66bcc52d547a3b33793fd0965d
Louise Beh <louise.beh@gmail.com>
2015-02-11 19:55:15 +0000

    Updated feature: board +(test spec)

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,15 @@
 #module Game
 class Board
 
-attr_reader :grid
-
-  def initialize
-    @board
-  end
-
-
-def grid
-  @board = {}
-  ('a'..'b').map do |letter|
-    (1..2).map do |i|
-      @board.store["#{letter}#{i}"] = Cell.new
+  def grid
+    @board = {}
+    ('a'..'b').map do |letter|
+      (1..2).map do |i|
+        @board.store["#{letter}#{i}"] = Cell.new
+      end
     end
+    board
   end
-board
-end
-
 
 
 end
